deb8f93338e26f94232e825d4d822892fec59774
DevThet <DevThet@hotmail.com>
2021-12-31 14:28:38 +0630

    Api Key base Authentication

---
+++ Tweet-Book/Controllers/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweet_Book.Filters;
+
+namespace Tweet_Book.Controllers
+{
+   
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secret.");
+        }
+    }
+}
